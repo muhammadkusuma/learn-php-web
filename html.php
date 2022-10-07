55831c6f4621f0bb646976c391cc5f1fac4f37f3
muhammadkusuma <mwiraadekusuma1@gmail.com>
2022-10-07 07:30:19 +0700

    Integrasi dengan HTML

---
+++ html.php
@@ -0,0 +1,15 @@
+<?php
+    $title="Hello PHP dan HTML";
+    $body="hello php dan html";
+?>
+
+<html>
+<head>
+    <title><?=$title?></title>
+</head>
+<body>
+<h1>
+    <?= $body;?>
+</h1>
+</body>
+</html>
