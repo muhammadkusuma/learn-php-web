a1a78e4d399b1a4cc3b5e54c8d399bd92bb9801a
muhammadkusuma <mwiraadekusuma1@gmail.com>
2022-10-09 14:36:33 +0700

    Response Code

---
+++ get-validation.php
@@ -0,0 +1,12 @@
+<?php
+if (!isset($_GET['name'])|| $_GET['name'] == ""){
+    http_response_code(400);
+    echo "Name is Required";
+    exit();
+}
+?>
+<html>
+<body>
+<h1><?=$say?></h1>
+</body>
+</html>
